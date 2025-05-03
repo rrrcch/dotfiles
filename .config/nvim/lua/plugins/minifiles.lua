caecc86924d6f9c5ef8f48b80b0bc81482a171c2
Ricardo Recchia <r.recchia@gmail.com>
2025-05-03 19:19:56 -0300

    feat(nvim): add mini.files

---
+++ .config/nvim/lua/plugins/minifiles.lua
@@ -0,0 +1,13 @@
+return {
+  "echasnovski/mini.files",
+  opts = {
+    mappings = {
+      close = "q",
+      go_in_plus = "<CR>",
+      go_out_plus = "<BS>",
+      reset = "<Esc>",
+      reveal_cwd = "@",
+      synchronize = "=",
+    },
+  },
+}
